d08206183254c2be986d9097796b194cbaaa26d7
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-06-14 18:58:44 +0800

    fix toc incorrectly assuming lines starting with `#` that are within code blocks are also headings

--- src/util/toc.rs
+++ src/util/toc.rs
@@ -1,8 +1,20 @@
 pub fn generate_toc_html(markdown: String) -> String {
     let mut toc = String::new();
     let mut toc_depth = 0;
+    let mut tags: Vec<String> = Vec::new();
+
     for line in markdown.lines() {
-        if line.starts_with("#") {
+        if line.starts_with("```") && tags.last().unwrap_or(&"".to_string()) != "```" {
+            tags.push("```".to_string());
+            continue;
+        }
+
+        if line.starts_with("```") && tags.last().unwrap_or(&"".to_string()) == "```" {
+            tags.pop();
+            continue;
+        }
+
+        if line.starts_with('#') && tags.len() == 0 {
             let depth = line.chars().take_while(|c| *c == '#').count();
             let heading = line[depth..].trim();
             let heading_id = heading.to_lowercase().replace(" ", "-");
